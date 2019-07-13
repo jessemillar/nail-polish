5f3a90c76c9191e781e8b824dda6138189a97ff0
Jesse Millar <hellojessemillar@gmail.com>
2019-07-13 18:14:48 -0500

    Updating the database finally

--- scripts/database.js
+++ scripts/database.js
@@ -1,124 +1,197 @@
 var database = [{
-	id: 19,
-	name: "white page",
-}, {
-	id: 32,
-	name: "tuck it in my tux",
-}, {
-	id: 942,
-	name: "penny talk",
-}, {
-	id: 1129,
-	name: "leggy legend",
-}, {
-	id: 672,
-	name: "mochacino",
-}, {
-	id: 1131,
-	name: "frock 'n roll",
-}, {
-	id: 0,
-	name: "top coat",
-}, {
-	id: 30,
-	name: "sew me",
-}, {
-	id: 170,
-	name: "beauty nap",
-}, {
-	id: 380,
-	name: "off duty style",
-}, {
-	id: 320,
-	name: "find me a man-nequin",
-}, {
-	id: 96,
-	name: "ballet slippers",
-}, {
-	id: 1125,
-	name: "peach side babe",
-}, {
-	id: 608,
-	name: "in stitches",
-}, {
-	id: 1132,
-	name: "with the band",
-}, {
-	id: 1133,
-	name: "in the lobby",
-}, {
-	id: 416,
-	name: "berry naughty",
-}, {
-	id: 250,
-	name: "looks to thrill",
-}, {
-	id: 1154,
-	name: "off tropic",
-}, {
-	id: 1130,
-	name: "bell-bottom blues",
-}, {
-	id: 754,
-	name: "mint candy apple",
-}, {
-	id: 784,
-	name: "aruba blue",
-}, {
-	id: 1135,
-	name: "virgin snow",
-}, {
-	id: 310,
-	name: "go ginza",
-}, {
-	id: 748,
-	name: "naughty nautical",
-}, {
-	id: 770,
-	name: "pret-a-surfer",
-}, {
-	id: 536,
-	name: "geranium",
-}, {
-	id: 1162,
-	name: "nama-stay the night",
-}, {
-	id: 757,
-	name: "chillato",
-}, {
-	id: 264,
-	name: "watermelon",
-}, {
-	id: 276,
-	name: "flowerista",
-}, {
-	name: "love sheen",
-}, {
-	name: "no shrinking violet",
-}, {
-	name: "muse, myself",
-}, {
-	id: 1171,
-	name: "raggae and rum"
-}, {
-	id: 350,
-	name: "gala-vanting"
-}, {
-	id: 80,
-	name: "twill seeker"
-}, {
-	id: 400,
-	name: "caviar bar"
-}, {
-	id: 20,
-	name: "spool me over"
-}, {
-	id: 100,
-	name: "pearls of wisdom"
-}, {
-	id: 42,
-	name: "satin slipper"
-}, {
-	id: 164,
-	name: "closing night"
-}];
+    id: 19,
+    name: "white page",
+  }, {
+    id: 32,
+    name: "tuck it in my tux",
+  }, {
+    id: 942,
+    name: "penny talk",
+  }, {
+    id: 1129,
+    name: "leggy legend",
+  }, {
+    id: 672,
+    name: "mochacino",
+  }, {
+    id: 1131,
+    name: "frock 'n roll",
+  }, {
+    id: 0,
+    name: "top coat",
+  }, {
+    id: 30,
+    name: "sew me",
+  }, {
+    id: 170,
+    name: "beauty nap",
+  }, {
+    id: 380,
+    name: "off duty style",
+  }, {
+    id: 320,
+    name: "find me a man-nequin",
+  }, {
+    id: 96,
+    name: "ballet slippers",
+  }, {
+    id: 1125,
+    name: "peach side babe",
+  }, {
+    id: 608,
+    name: "in stitches",
+  }, {
+    id: 1132,
+    name: "with the band",
+  }, {
+    id: 1133,
+    name: "in the lobby",
+  }, {
+    id: 416,
+    name: "berry naughty",
+  }, {
+    id: 250,
+    name: "looks to thrill",
+  }, {
+    id: 1154,
+    name: "off tropic",
+  }, {
+    id: 1130,
+    name: "bell-bottom blues",
+  }, {
+    id: 754,
+    name: "mint candy apple",
+  }, {
+    id: 784,
+    name: "aruba blue",
+  }, {
+    id: 1135,
+    name: "virgin snow",
+  }, {
+    id: 310,
+    name: "go ginza",
+  }, {
+    id: 748,
+    name: "naughty nautical",
+  }, {
+    id: 770,
+    name: "pret-a-surfer",
+  }, {
+    id: 536,
+    name: "geranium",
+  }, {
+    id: 1162,
+    name: "nama-stay the night",
+  }, {
+    id: 757,
+    name: "chillato",
+  }, {
+    id: 264,
+    name: "watermelon",
+  }, {
+    id: 276,
+    name: "flowerista",
+  }, {
+    name: "love sheen",
+  }, {
+    name: "no shrinking violet",
+  }, {
+    name: "muse, myself",
+  }, {
+    id: 1171,
+    name: "raggae and rum"
+  }, {
+    id: 350,
+    name: "gala-vanting"
+  }, {
+    id: 80,
+    name: "twill seeker"
+  }, {
+    id: 400,
+    name: "caviar bar"
+  }, {
+    id: 20,
+    name: "spool me over"
+  }, {
+    id: 100,
+    name: "pearls of wisdom"
+  }, {
+    id: 42,
+    name: "satin slipper"
+  }, {
+    id: 164,
+    name: "closing night"
+  },
+  {
+    id: 40,
+    name: "fairy tailor"
+  },
+  {
+    id: 560,
+    name: "star studded"
+  },
+  {
+    id: 540,
+    name: "to have & to gold"
+  }, {
+    id: 00,
+    name: "top coat"
+  }, {
+    id: 135,
+    name: "first view"
+  }, {
+    id: 1558,
+    name: "soles on fire"
+  }, {
+    id: 1540,
+    name: "and mercury on"
+  }, {
+    name: "knee-high life"
+  }, {
+    name: "dressed to the nineties"
+  }, {
+    name: "mixtaupe"
+  }, {
+    name: "good lighting"
+  }, {
+    name: "pinked to perfection"
+  }, {
+    name: "on the mauve"
+  }, {
+    name: "indi-go for it!"
+  }, {
+    name: "a-game"
+  }, {
+    name: "dulce amore",
+    brand: "ella + mila"
+  }, {
+    name: "ibiza breeze",
+
+    brand: "ella + mila"
+  },
+  {
+    name: "mediterranean mist",
+    brand: "ella + mila"
+  },
+  {
+    name: "all about the base",
+    brand: "ella + mila"
+  }, {
+    name: "matte-ly in love",
+    brand: "ella + mila"
+  },
+  {
+    name: "good to go topcoat"
+  }, {
+    name: "purity",
+    brand: "zoya"
+  },
+  {
+    name: "jack",
+    brand: "zoya"
+  },
+  {
+    name: "sonja",
+    brand: "zoya"
+  }
+];
